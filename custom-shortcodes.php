825c4ce716a1a0739321c6b296429d44a8739372
carlofabyss <carlofabyss@hotmail.com>
2022-02-04 17:42:03 -0800

    Removed old navigation

--- custom-shortcodes.php
+++ custom-shortcodes.php
@@ -1,11 +1,11 @@
 <?php
 
-    function sdigital_thebubble() {
-        $result = '<button id="sdigital_nav_button" class="sdigital_nav_button" type="button"><img src="' . get_stylesheet_directory_uri() . '/assets/svg/menu.svg"></button>';
+    // function sdigital_thebubble() {
+    //     $result = '<button id="sdigital_nav_button" class="sdigital_nav_button" type="button"><img src="' . get_stylesheet_directory_uri() . '/assets/svg/menu.svg"></button>';
 
-        return $result;
+    //     return $result;
             
-    }
+    // }
 
-    add_shortcode('showthebubble', 'sdigital_thebubble');
+    // add_shortcode('showthebubble', 'sdigital_thebubble');
 ?>
