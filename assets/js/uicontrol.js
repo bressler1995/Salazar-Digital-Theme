a4e4dbd27eb2b94e39f159955fbae520b3eb0858
carlofabyss <carlofabyss@hotmail.com>
2022-02-17 12:58:49 -0800

    Remove empty iframes

--- assets/js/uicontrol.js
+++ assets/js/uicontrol.js
@@ -1,3 +1,20 @@
-jQuery( document ).ready(function() {
-    console.log("Welcome to Salazar Digital...");
+jQuery(document).ready(function($){
+	console.log("Loaded!");
+	let the_portvideo = document.getElementById("the_portvideo");
+	
+	if(the_portvideo != null) {
+	   setTimeout(function(){
+		   console.log(the_portvideo.innerHTML.includes("iframe"));
+		   let theinner = the_portvideo.innerHTML;
+		   console.log(theinner);
+		   
+			if(theinner.includes('iframe') != true) {
+				console.log("No Iframes Detected...");
+				if(the_portvideo.classList.contains("hideportvideo") == false) {
+						the_portvideo.classList.add("hideportvideo");
+				}
+			}
+	   }, 1000);
+}
+
 });
