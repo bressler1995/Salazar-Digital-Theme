b9b467300fb12a186dd4fdb70d48ecb87eb199d0
carlofabyss <carlofabyss@hotmail.com>
2022-03-22 15:57:51 -0700

    Fix issue with scrolling videos

--- custom-shortcodes.php
+++ custom-shortcodes.php
@@ -8,4 +8,31 @@
     // }
 
     // add_shortcode('showthebubble', 'sdigital_thebubble');
+
+    function sdigital_vimeofunction() {
+        $result = '';
+        $the_id = get_the_ID();
+        
+        if($the_id != false) {
+            $scrolling_video = get_field("scrolling_video", $the_id);
+
+            if( $scrolling_video ) {
+                if(str_contains($scrolling_video, 'https://vimeo.com/')) {
+                    $scrolling_video_array = explode("https://vimeo.com/", $scrolling_video);
+                    if(empty($scrolling_video_array) == false) {
+                        if(count($scrolling_video_array) == 2) {
+                            $result .= '<div class="sdigital_vimeocontainer">
+                                <iframe src="https://player.vimeo.com/video/' . $scrolling_video_array[1] . '?background=1&autoplay=1&loop=1&byline=0&title=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                            </div>';
+                        }
+                    }
+                }
+            }
+
+        }
+
+        return $result;
+    }
+
+    add_shortcode('showvimeo', 'sdigital_vimeofunction');
 ?>
